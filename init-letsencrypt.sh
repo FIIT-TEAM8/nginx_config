20fbe9f1ad696a87715e9b263133832ee2cfd901
AMS <you@example.com>
2022-09-24 15:44:59 +0200

    updated configuration

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,9 +5,9 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(team08-21.studenti.fiit.stuba.sk)
+domains=(adversea.com)
 rsa_key_size=4096
-data_path="../certbot"
+data_path="./data_link/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
